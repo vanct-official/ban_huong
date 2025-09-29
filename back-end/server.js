54fc788292bb0002fabd38d5ce61c81a18ef0e56
Chu Thế Văn <chuthevan450@gmail.com>
2025-09-29 21:06:05 +0700

    Update server.js

--- back-end/server.js
+++ back-end/server.js
@@ -36,7 +36,25 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://ban-huong.vercel.app", // frontend deploy trên Vercel
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true); // allow Postman or server-to-server requests
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = `CORS policy: ${origin} not allowed`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+  })
+);
+
 app.use(
   session({
     secret: process.env.SESSION_SECRET || "secret",
